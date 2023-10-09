f39b9b4bcf72f4cd89147c76da9e15c3ae5155cd
PhilipSoliman <p.m.soliman@student.tudelft.nl>
2023-10-09 17:54:20 +0200

    updated print statements

--- data_extraction/saveArray.c
+++ data_extraction/saveArray.c
@@ -23,17 +23,17 @@ void saveArray(double myArray[][2], int arraySize, char fullPath[]){
     // creating directory (read/write/search permissions for owner and group, and with read/search permissions for others)
     printf("Creating directory %s\n...", directoryPath);
     if (mkdir(directoryPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == 0){
-        printf("Directory created!\n");
+        printf("\tDirectory created!\n");
     } else if (errno == EEXIST){
-        printf("Directory already exists!\n");
+        printf("\tDirectory already exists!\n");
+        exit(1);
     } else if (errno == ENOENT){
-        printf("Part of directory does not exist!\n");
+        printf("\tPart of directory does not exist!\n");
+        exit(1);
     } else {
-        printf("Error creating directory: %s, errno = %i\n", strerror(errno), errno);
+        printf("\tError creating directory: %s, errno = %i\n", strerror(errno), errno);
+        exit(1);
     }
-    // if (stat(directoryPath, &st) == -1) {
-    //     mkdir(directoryPath, 0700);
-    // }
 
     // opening file  
     printf("Opening file at %s\n", fullPathCopy);
@@ -42,9 +42,10 @@ void saveArray(double myArray[][2], int arraySize, char fullPath[]){
     }
 
     // writing to file
-    printf("Saving %s to folder %s\n", fileName, directoryPath);
+    printf("Writing %s to folder %s\n", fileName, directoryPath);
     if(fwrite(myArray, sizeof(float), arraySize, fp) != arraySize){
         printf("File write error.");
+        exit(1);
     }
 
     // closing file 
