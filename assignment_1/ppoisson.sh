543e61f35ffa6fb847d8cb1ad1a2731979db4198
PhilipSoliman <p.m.soliman@student.tudelft.nl>
2024-01-11 17:57:07 +0100

    assignment 1 start

---
+++ assignment_1/ppoisson.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+#SBATCH --job-name="ppoisson"
+#SBATCH --partition=compute
+#SBATCH --time=0:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=3
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=Education-EEMCS-Courses-IN4049TU
+#SBATCH --output=/home/%u/HPC/hpc-labs/out/assignment_1/%x.out
+#SBATCH --error=/home/%u/HPC/hpc-labs/out/assignment_1/%x.err
+
+
+module load 2023r1
+module load openmpi
+
+cd ~/HPC/hpc-labs/assignment_1/
+
+mpicc ppoisson.c -o ppoisson.x
+srun ppoisson.x 1 3
