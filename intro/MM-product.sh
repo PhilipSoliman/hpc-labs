404ccd1cacd367748ee4bbc5a60ac1d1bb17d3db
PhilipSoliman <p.m.soliman@student.tudelft.nl>
2024-01-24 13:10:46 +0100

    new output

--- intro/MM-product.sh
+++ intro/MM-product.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name="MM-product"
 #SBATCH --partition=compute
 #SBATCH --time=0:01:00
-#SBATCH --nodes=2
-#SBATCH --ntasks=64
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=Education-EEMCS-Courses-IN4049TU
