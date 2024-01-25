e06bdd4f49deff8430833e2603df7a88af7e45b5
Philip Soliman <philipsoliman4133@gmail.com>
2024-01-25 22:35:33 +0100

    report update

--- intro/analysis.py
+++ intro/analysis.py
@@ -42,14 +42,26 @@ ax.set_ylabel("Time (s)")
 ax.grid(True)
 
 for i in range(len(pingPongTimes)):
-    ax.plot(
-        pingPongTimes[i, :, 0],
-        pingPongTimes[i, :, 1],
+    x = pingPongTimes[i, :, 0]
+    y = pingPongTimes[i, :, 1]
+
+    lines = ax.plot(
+        x,
+        y,
         label=f"#nodes = {pingPongMeta[i]['nnodes']}",
+        marker="x",
+        linestyle=""
     )
 
+
+    # numpy polyfit
+    p = np.polyfit(x, y, 1)
+    y_fit = np.polyval(p, x)
+    ax.plot(x, y_fit, label="fit", linestyle="--", color=lines[0].get_color())
+
+
 ax.legend()
-# plt.show()
+plt.show()
 plt.tight_layout()
 filename = f"pingPong_times.png"
 filepath = root / "report" / "figures" / filename
@@ -66,38 +78,53 @@ header = [
 caption = "Execution time and speedup for matrix multiplication"
 MMTimes_T = MMTimes.transpose(2, 0, 1)  # matrix size, processes, time
 Nsizes, Nprocs, _ = MMTimes_T.shape
-rows = [[]]*(Nsizes*(Nprocs+1)+1)
+rows = [[]] * (Nsizes * (Nprocs + 1) + 1)
 rows[0] = header
 procs = sorted([int(MMMeta[i]["nproc"]) for i in range(len(MMMeta))])
+
 
 def scientific_fmt(s: float, prec: int = 2) -> str:
     specifier = f"{{:.{prec}e}}"
     scientific_str = specifier.format(s)
     mantissa, exponent = scientific_str.split("e")
-    out = mantissa + r"$\times 10^{" + exponent + "}$"
+    if exponent[0] == "+":
+        sign = ""
+    elif exponent[0] == "-":
+        sign = "-"
+    if exponent[1] == "0":
+        exponent = exponent[2:]
+    if exponent == "0":
+        out = mantissa
+    else:
+        out = mantissa + r"$\times 10^{" + sign + exponent + "}$"
     return out
+
 
 for i in range(len(MMTimes_T)):
     measurements = MMTimes_T[i]
     for j, [seq, par, speedup, size] in enumerate(measurements):
         nnodes, nproc = str(MMMeta[j]["nnodes"]), str(MMMeta[j]["nproc"])
-        proc_idx = procs.index(int(nproc))+1
-        if j==0:
+        proc_idx = procs.index(int(nproc)) + 1
+        if j == 0:
             size_str = r"\hline" + f"{size:.0f}" + r"$\times$" + f"{size:.0f}"
             # size_str = f"{size:.0f}" + r"$\times$" + f"{size:.0f}"
-            rows[i*(Nprocs+1)+1] = [size_str, 1, nnodes, seq, ""]
-        row_index = i*Nprocs+proc_idx+1
-        rows[i*(Nprocs+1)+proc_idx+1] = ["", nproc, nnodes, par, speedup]
+            rows[i * (Nprocs + 1) + 1] = [size_str, 1, nnodes, seq, 1]
+        row_index = i * Nprocs + proc_idx + 1
+        rows[i * (Nprocs + 1) + proc_idx + 1] = ["", nproc, nnodes, par, speedup]
 table = Texttable()
 table.set_cols_align(["c"] * len(header))
 table.set_deco(Texttable.HEADER | Texttable.VLINES)
 table.set_precision(2)
-table.add_rows(rows)
 table.set_cols_dtype(["i", "t", "t", scientific_fmt, scientific_fmt])
+table.add_rows(rows)
 label = f"tab:{header[0]}"
+
+# add position specifier
+table_str = latextable.draw_latex(table, caption=caption, label=label)
+table_str = table_str.replace(r"\begin{table}", r"\begin{table}[H]")
 
 
 filename = "MM_times_table.tex"
 filepath = root / "report" / "tables" / filename
 with open(filepath, "w") as f:
-    f.write(latextable.draw_latex(table, caption=caption, label=label))
+    f.write(table_str)
