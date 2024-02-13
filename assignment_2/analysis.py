dad25dd6dc4ba9cbce2adc01b3631c2978ff0458
Philip Soliman <philipsoliman4133@gmail.com>
2024-02-13 17:30:57 +0100

    final update to ass2

--- assignment_2/analysis.py
+++ assignment_2/analysis.py
@@ -33,33 +33,39 @@ for i, file in enumerate(outputFiles):
         number_of_vertices, 3
     )
 
-pprint(outputData)
 # 3d surface plot
 fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-data = outputData[6]
-phi = data["phi"]
-ax.plot_trisurf(
-    phi[:, 0],
-    phi[:, 1],
-    phi[:, 2],
-    label=f"{i}",
-    antialiased=False,
-    cmap=cm.coolwarm,
-    shade=True,
-    linewidth=0.01,
-    edgecolor="black",
-)
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("$\phi$")
-
+for i, data in outputData.items():
+    grid = data["meta"]["grid"]
+    adapt = int(data["meta"]["adapt"])
+    if grid == "100x100" and adapt == 0:
+        procg = data["meta"]["procg"]
+        if procg == "2x2":
+            ax = fig.add_subplot(121, projection="3d")
+            ax.set_xlabel("X")
+            ax.set_ylabel("Y")
+            ax.set_zlabel("$\phi$")
+            ax.set_title(f"{procg}")
+        elif procg == "4x1":
+            ax = fig.add_subplot(122, projection="3d")
+            ax.set_title(f"{procg}")
+        phi = data["phi"]
+        ax.plot_trisurf(
+            phi[:, 0],
+            phi[:, 1],
+            phi[:, 2],
+            antialiased=False,
+            cmap=cm.coolwarm,
+            shade=True,
+            linewidth=0.01,
+            edgecolor="black",
+        )
 plt.tight_layout()
 
 # save plot
 filename = f"fempoisson_surface.png"
 filepath = root / "report" / "figures" / filename
-fig.savefig(filepath, dpi=300, pad_inches=0.5, bbox_inches="tight")
+fig.savefig(filepath, dpi=500, pad_inches=0.5, bbox_inches="tight")
 
 
 # benchmark folder
@@ -78,8 +84,228 @@ for i, file in enumerate(benchmarkFiles):
         benchmarkData[i]["times"] = np.fromfile(file, dtype=float).reshape(
             number_of_processors, len(cols)
         )
-    elif meta["type"] == "errors":
-        benchmarkData[i]["errors"] = np.fromfile(file, dtype=float)
+    elif meta["type"] == "error":
+        benchmarkData[i]["error"] = np.fromfile(file, dtype=float)
 
+# stack bar plot for benchmark times
+grids = ["100x100", "200x200", "400x400"]
+fig, axs = plt.subplots(nrows=3, ncols=2, figsize=(10, 15), sharex=True)
+width = 0.5
+
+for i, data in benchmarkData.items():
+    number_of_processors = int(data["meta"]["nproc"])
+    x = np.arange(number_of_processors)
+    bottom = np.zeros(number_of_processors)
+    meta = data["meta"]
+    grid = meta["grid"]
+    adapt = int(meta["adapt"])
+    procg = meta["procg"]
+    if meta["type"] == "times" and adapt == 0:
+        times = data["times"]
+        if grid in grids:
+            rowidx = grids.index(grid)
+        if procg == "2x2":
+            ax = axs[rowidx, 0]
+            ax.set_title(f"{grid}", loc="left")
+        elif procg == "4x1":
+            ax = axs[rowidx, 1]
+        for j, col in enumerate(cols):
+            ax.bar(
+                x,
+                times[:, j],
+                width,
+                label=col,
+                bottom=bottom,
+            )
+            bottom += times[:, j]
+        if rowidx == 0:
+            ax.set_title(f"{procg}")
+
+# place legend in the
+axs[0, 0].legend(fontsize=10)
+axs[0, 0].set_xlabel("Process rank")
+axs[0, 0].set_ylabel("Time (s)")
+plt.tight_layout()
+
+# save plot
+filename = f"fempoisson_benchmark.png"
+filepath = root / "report" / "figures" / filename
+fig.savefig(filepath, dpi=300, pad_inches=0.5, bbox_inches="tight")
+
+# computation and communication overlap estimate (fixed number of processors)
+grids = {0: {}, 1: {}}
+overlaps = {0: {}, 1: {}}
+for i, data in benchmarkData.items():
+    meta = data["meta"]
+    procg = meta["procg"]
+    gridsize = int(meta["grid"].split("x")[0])
+    if gridsize > 100:
+        continue
+    adapt = int(meta["adapt"])
+    if meta["type"] == "times":
+        if grids[adapt].get(procg) is None:
+            grids[adapt][procg] = [gridsize]
+        else:
+            grids[adapt][procg].append(gridsize)
+        times = data["times"]
+        computation = times[:, 0]
+        communication = times[:, 2] + times[:, 1]
+        overlap = np.sum(computation) / np.sum(communication)  # sum over all processors
+        if overlaps[adapt].get(procg) is None:
+            overlaps[adapt][procg] = [overlap]
+        else:
+            overlaps[adapt][procg].append(overlap)
+
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), sharex=True, sharey=True)
+for adapt, data in grids.items():
+    for procg, grid in data.items():
+        ax = axs[adapt]
+        if adapt == 0:
+            marker = "x"
+            ax.set_title("Non-adaptive")
+        else:
+            marker = "o"
+            ax.set_title("Adaptive")
+        ls = ax.plot(
+            grid, overlaps[adapt][procg], label=procg, marker=marker, linestyle="None"
+        )
+
+        if adapt == 0:
+            # linear fit
+            x = np.array(grid)
+            y = np.array(overlaps[adapt][procg])
+            p = np.polyfit(x, y, 1)
+            y_fit = np.polyval(p, np.sort(x))
+            ax.plot(np.sort(x), y_fit, linestyle="-", color=ls[0].get_color())
+
+# labels
+axs[0].plot(x, np.ones(len(x)), "--r")
+axs[1].plot(x, np.ones(len(x)), "--r")
+axs[0].set_xlabel("Grid size")
+axs[0].set_ylabel("Overlap")
+axs[0].legend()
+
+plt.tight_layout()
+
+# save plot
+filename = f"fempoisson_overlap_vs_gridsize.png"
+filepath = root / "report" / "figures" / filename
+fig.savefig(filepath, dpi=300, pad_inches=0.5, bbox_inches="tight")
+
+# communication overlap estimate (fixed grid size)
+pgrids = {0: [], 1: []}
+overlaps = {0: [], 1: []}
+for i, data in benchmarkData.items():
+    meta = data["meta"]
+    procg = meta["procg"]
+    pgridsize = int(meta["procg"].split("x")[0])
+    gridsize = int(meta["grid"].split("x")[0])
+    if gridsize != 1000:
+        continue
+    adapt = int(meta["adapt"])
+    if meta["type"] == "times":
+        pgrids[adapt].append(pgridsize)
+        times = data["times"]
+        computation = times[:, 0]
+        communication = times[:, 2] + times[:, 1]
+        overlap = np.sum(computation) / np.sum(communication)  # sum over all processors
+        overlaps[adapt].append(overlap)
+
+pprint(pgrids)
+pprint(overlaps)
+
+fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), sharex=True, sharey=True)
+for adapt, pgrid in pgrids.items():
+    ax = axs[adapt]
+    if adapt == 0:
+        marker = "x"
+        ax.set_title("Non-adaptive")
+    else:
+        marker = "o"
+        ax.set_title("Adaptive")
+    ls = ax.plot(pgrid, overlaps[adapt], label=procg, marker=marker, linestyle="None")
+
+    if adapt == 0:
+        # linear fit
+        x = np.array(pgrid)
+        y = np.array(overlaps[adapt])
+        p = np.polyfit(x, y, 1)
+        y_fit = np.polyval(p, np.sort(x))
+        ax.plot(np.sort(x), y_fit, linestyle="-", color=ls[0].get_color())
+
+# labels
+axs[0].plot(x, np.ones(len(x)), "--r")
+axs[1].plot(x, np.ones(len(x)), "--r")
+axs[0].set_xlabel("number of processes")
+axs[0].set_ylabel("Overlap")
+axs[0].legend()
+
+plt.tight_layout()
+
+# save plot
+filename = f"fempoisson_overlap_vs_processes.png"
+filepath = root / "report" / "figures" / filename
+fig.savefig(filepath, dpi=300, pad_inches=0.5, bbox_inches="tight")
+
+# error evolution plot
+grids = ["100x100", "200x200", "400x400"]
+pgrids = ["2x2", "4x1"]
+adapt_times = np.zeros((len(grids), len(pgrids)))
+nadapt_times = np.zeros((len(grids), len(pgrids)))
+total_times = {}
+fig, axs = plt.subplots(
+    nrows=len(grids), ncols=len(pgrids), figsize=(10, 15), sharey=True
+)
+for i, data in benchmarkData.items():
+    meta = data["meta"]
+    grid = meta["grid"]
+    pgrid = meta["procg"]
+    times = data.get("times")
+    if times is not None:
+        total_time = np.sum(times) / 4
+    if meta["type"] != "error":
+        continue
+    if grid in grids:
+        ridx = grids.index(grid)
+        cidx = pgrids.index(pgrid)
+        ax = axs[ridx][cidx]
+        error = data["error"]
+        if meta["adapt"] == "0":
+            adapt_times[ridx][cidx] = total_time
+            if cidx == 0:
+                ax.plot(error, label="non-adaptive")
+            else:
+                ax.plot(error)
+        if meta["adapt"] == "1":
+            nadapt_times[ridx][cidx] = total_time
+            if cidx == 0:
+                ax.plot(error, label="adaptive")
+            else:
+                ax.plot(error)
+        ax.set_yscale("log")
+        if cidx == 0 and ridx == 0:
+            ax.set_ylabel("Error")
+            ax.set_xlabel("Iterations")
+            ax.legend()
+        if cidx == 0:
+            ax.set_title(f"{grid}", loc="left")
+        if ridx == 0:
+            ax.set_title(f"{pgrid}", loc="center")
+# add delta time
+for i in range(len(grids)):
+    for j in range(len(pgrids)):
+        axs[i][j].set_title(
+            f"{axs[i][j].get_title()}\n$\Delta t = {adapt_times[i][j] - nadapt_times[i][j]:.2f} s$",
+            loc="right",
+            fontsize=12,
+        )
+print(adapt_times)
+print(nadapt_times)
+# save plot
+filename = f"fempoisson_error_evolution.png"
+filepath = root / "report" / "figures" / filename
+fig.savefig(filepath, dpi=300, pad_inches=0.5, bbox_inches="tight")
+
+plt.tight_layout()
 if args_d.get("output") or __name__ == "__main__":
     plt.show()
